805ea4627857da9d97270a24fa842ad4f6ab8191
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2024-04-12 09:43:49 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import App from './App';
+import { render } from '@testing-library/react';
 
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
